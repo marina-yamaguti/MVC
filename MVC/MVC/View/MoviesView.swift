d3732a30574a70198f7598a4ab21169567b32f12
Waldyr Schneider <Waldyr.Schneider@edu.pucrs.br>
2024-03-20 14:47:31 -0300

    refactor: refactoring tableview

---
+++ MVC/MVC/View/MoviesView.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Marina Yamaguti on 18/03/24.
+//
+
+import UIKit
+import Combine
+
+class MoviesView: UIViewController, UITableViewDataSource {
+    
+    //MARK: UITableView Sections
+
+    
+    private let tableView = UITableView()
+    
+    private var controller: MoviesViewController  = MoviesViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.controller.fetchAllMovies(tableView: tableView)
+        configViews()
+    }
+
+    
+    private func configViews() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.register(MovieCell.self, forCellReuseIdentifier: "MovieCell")
+        tableView.rowHeight = 118
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - TableView DataSource
+extension MoviesView {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return controller.sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return controller.sections[section].value
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let currentSection = controller.sections[section]
+        
+        switch currentSection {
+        case .nowPlaying:
+            return controller.nowPlaying.count
+        case .popular:
+            return controller.popular.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currentSection = controller.sections[indexPath.section]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MovieCell else { fatalError() }
+        var movie: Movie
+        
+        switch currentSection {
+        case .nowPlaying:
+            movie = controller.nowPlaying[indexPath.row]
+        case .popular:
+            movie = controller.popular[indexPath.row]
+        }
+        
+        controller.configureCell(cell, with: movie)
+    
+        return cell
+    }
+}
+
